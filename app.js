76798f0b9995b034f356af6b2087245db842970a
Mohammed Ziyad <ziyad@itproguide.com>
2022-02-02 07:39:59 +0530

    port bug fixed

--- app.js
+++ app.js
@@ -180,6 +180,6 @@ app.get("/cal", function(req,res){
 
 
 
-app.listen("3000", function(){
-    console.log("server started at port 3000");
-})
+app.listen(process.env.PORT ||3000, function(){
+    console.log("server is running in port 3000");
+});
